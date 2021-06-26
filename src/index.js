19fd431c14b3538ba22d6258149a5831f4b85f0e
elver tobo <elvertobo2017@gmail.com>
2021-06-26 16:55:34 -0500

    Se crean variables de control para los sensores a traves de hooks de tipo useState

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dashboard from './Dashboard';
 import DashboardScreen from './DashboardScreen';
 import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <DashboardScreen />
+    <DashboardScreen/>
   </React.StrictMode>,
   document.getElementById('root')
 );
